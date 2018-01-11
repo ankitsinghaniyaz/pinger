0db594eae87154db7814f1863d32576f37b151c7
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2018-01-11 14:47:56 +0530

    put the code execution under a main method, to avoid direct execution on load

--- lib/pinger.rb
+++ lib/pinger.rb
@@ -8,14 +8,16 @@ require "pinger/client"
 # Pinger is a library to probe any url for a given amount of time
 # in specified time interval and compute the mean response time from the server
 module Pinger
-  parser = Pinger::Parser.new(ARGV)
-  options, website = parser.parse!
+  def self.main(args)
+    parser = Pinger::Parser.new(args)
+    options, website = parser.parse!
 
-  duration = options[:duration]
-  interval = options[:interval]
+    duration = options[:duration]
+    interval = options[:interval]
 
-  client = Pinger::Client.new(website, duration, interval)
-  mean_response_time = client.compute_mean_response
+    client = Pinger::Client.new(website, duration, interval)
+    mean_response_time = client.compute_mean_response
 
-  puts "#{website} mean response time for #{duration/interval} trips: #{mean_response_time.round(2)} ms"
+    puts "#{website} mean response time for #{duration/interval} trips: #{mean_response_time.round(2)} ms"
+  end
 end
