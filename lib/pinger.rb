30ce3449dd08338bfce793e370ec84e892790a37
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2018-01-11 13:26:29 +0530

    complete compute and dispaly of result in pinger

--- lib/pinger.rb
+++ lib/pinger.rb
@@ -1,5 +1,17 @@
 require "pinger/version"
+require "pinger/exit_code"
+require "pinger/parser"
+require "pinger/client"
 
 module Pinger
-  # Your code goes here...
+  parser = Pinger::Parser.new(ARGV)
+  options, website = parser.parse!
+
+  duration = options[:duration]
+  interval = options[:interval]
+
+  client = Pinger::Client.new(website, duration, interval)
+  mean_response_time = client.compute_mean_response
+
+  puts "#{website} mean response time for #{duration/interval} trips: #{mean_response_time.round(2)} ms"
 end
